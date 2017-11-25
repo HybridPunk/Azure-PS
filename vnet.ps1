353ded4ab87c619e5110334fd8cad8b4d0d8e44f
HybridPunk <33721934+HybridPunk@users.noreply.github.com>
2017-11-25 18:18:00 +0000

    rdh

---
+++ vnet.ps1
@@ -0,0 +1,9 @@
+New-AzureRmVirtualNetwork -
+
+New-AzureRmVirtualNetwork `
+    -AddressPrefix 192.168.1.0/24
+    -DnsServer <System.Collections.Generic.List`1[System.String]>]
+[-EnableDDoSProtection] [-EnableVmProtection] [-Force] -Location <String> -Name <String> -ResourceGroupName <String> [-Subnet <System.Collections.Generic.List`1[Microsoft.Azure.Commands.Network.Models.PSSubnet]>]
+[-Tag <Hashtable>] [-Confirm] [-WhatIf] [<CommonParameters>]
+
+
